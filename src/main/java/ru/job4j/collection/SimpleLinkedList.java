c05733b659d924c91a3e31b3a8e2e80c8c947f4b
iavn666 <dvoeglazovcom@gmail.com>
2022-06-06 13:00:04 +0500

    2. Создать контейнер на базе связанного списка [#159 #208448]

--- src/main/java/ru/job4j/collection/SimpleLinkedList.java
+++ src/main/java/ru/job4j/collection/SimpleLinkedList.java
@@ -1,7 +1,5 @@
 package ru.job4j.collection;
 
-import org.w3c.dom.Node;
-
 import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
@@ -13,24 +11,6 @@ public class SimpleLinkedList<E> implements LinkedList<E> {
     private Node<E> first;
     private Node<E> last;
     private int modCount;
-
-    public E node(int index) {
-        Objects.checkIndex(index, size);
-        Node<E> i = first;
-        Node<E> x = last;
-        Node<E> rsl = null;
-        if (index == 0) {
-            rsl = i;
-            return rsl.item;
-        } else if (index == (size - 1)) {
-            rsl = x;
-            return rsl.item;
-        } else if (index > 0) {
-            first.next = i;
-        }
-
-        return rsl.item;
-    }
 
 
     @Override
@@ -49,7 +29,20 @@ public class SimpleLinkedList<E> implements LinkedList<E> {
 
     @Override
     public E get(int index) {
-        return node(index);
+        Objects.checkIndex(index, size);
+        Node<E> i = first;
+        Node<E> x = last;
+        Node<E> rsl = null;
+        if (index == 0) {
+            rsl = i;
+            return rsl.item;
+        } else if (index == (size - 1)) {
+            rsl = x;
+            return rsl.item;
+        } else if (index > 0) {
+            first.next = i;
+        }
+        return rsl.item;
     }
 
     @Override
@@ -57,30 +50,26 @@ public class SimpleLinkedList<E> implements LinkedList<E> {
         return new Iterator<E>() {
 
             private final int expectedModCount = modCount;
-            private int cursor = 0;
-
+            private int num = 0;
 
             @Override
             public boolean hasNext() {
                 if (expectedModCount != modCount) {
                     throw new ConcurrentModificationException();
                 }
-                return cursor < size;
+                return size > num;
             }
 
             @Override
             public E next() {
-                if (!hasNext() || modCount == 0) {
+                if (!hasNext()) {
                     throw new NoSuchElementException();
                 }
                 SimpleLinkedList.Node<E> current = first;
-                for (int a = 0; a < cursor; a++) {
-                    while (current.next != null) {
-                        current = current.next;
-
-                    }
+                while ((current.next != null) && num > 0) {
+                    current = current.next;
                 }
-                cursor++;
+                num++;
                 return current.item;
             }
         };
